78deac11b00c6db678f6464cbda17c048d856d82
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-12-17 16:57:57 -0500

    adding logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
+import { Logo } from "../_components/logo";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center items-center h-full">{children}</div>
+    <div className="flex flex-col justify-center items-center h-full space-y-6">
+      <Logo />
+      {children}
+    </div>
   );
 };
 
