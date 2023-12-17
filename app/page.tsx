9a4f14a646adde00ec648a2e2c1648221122a40f
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-12-17 16:44:02 -0500

    adding authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-  return <Button size="lg">Click Me</Button>;
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
