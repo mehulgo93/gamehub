344d42de4e699345bbafb5a461adf47648c065e1
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-12-18 13:48:30 -0500

    finishing navbar

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -16,7 +16,7 @@ export const Search = () => {
 
     const url = qs.stringifyUrl(
       {
-        url: "/",
+        url: "/search",
         query: { term: value },
       },
       { skipEmptyString: true }
