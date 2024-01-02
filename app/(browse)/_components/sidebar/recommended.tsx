1c036dbcada2899eb06ef8a79dafd052195746ff
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-01-02 10:44:47 -0500

    new commit

---
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useSidebar } from "@/store/use-sidebar";
+import { User } from "@prisma/client";
+
+interface RecommendedProps {
+  data: User[];
+}
+
+export const Recommended = ({ data }: RecommendedProps) => {
+  const { collapsed } = useSidebar((state) => state);
+  const showLabel = !collapsed && data.length > 0;
+
+  return (
+    <div>
+      {showLabel && (
+        <div className="pl-6 mb-4">
+          <p className="text-sm text-muted-foreground">Recommended</p>
+        </div>
+      )}
+      <ul className="space-y-2 px-2">
+        {data.map((user) => {
+          <div key={user.id}>{user.username}</div>;
+        })}
+      </ul>
+    </div>
+  );
+};
