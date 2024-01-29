cd8f942175c666675e82a5d1a4ca4cea16ed925a
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-01-28 22:37:57 -0500

    adding streamplayer code

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,5 +1,25 @@
-const CreaterPage = () => {
-  return <div>Creater Page</div>;
+import { StreamPlayer } from "@/components/stream-player";
+import { getUserByUsername } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs";
+
+interface CreaterPageProps {
+  params: {
+    username: string;
+  };
+}
+
+const CreaterPage = async ({ params }: CreaterPageProps) => {
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(params.username);
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+  return (
+    <div className="h-full">
+      <StreamPlayer user={user} stream={user.stream} isFollowing />
+    </div>
+  );
 };
 
 export default CreaterPage;
