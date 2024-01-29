e024690f36f81c6955bcf941ead9bb4076101430
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-01-29 17:05:28 -0500

    stream player

--- components/stream-player.tsx
+++ components/stream-player.tsx
@@ -2,6 +2,7 @@
 
 import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
+import { LiveKitRoom } from "@livekit/components-react";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -19,5 +20,17 @@ export const StreamPlayer = ({
   if (!token || !name || !identity) {
     return <div>Cannot watch the stream</div>;
   }
-  return <div>Allowed to Watch the stream</div>;
+  return (
+    <>
+      <LiveKitRoom
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+      >
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollba pb-10">
+          <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+      </LiveKitRoom>
+    </>
+  );
 };
