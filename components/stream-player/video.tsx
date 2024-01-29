addc6dd0a5a7bc4e7f7b23bf60cee97470ee5dee
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-01-29 17:47:41 -0500

    adding more code to the video player

--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -1,10 +1,34 @@
 "use client";
 
+import { ConnectionState } from "livekit-client";
+import { Track } from "livekit-client";
+import {
+  useConnectionState,
+  useTracks,
+  useRemoteParticipant,
+} from "@livekit/components-react";
+import { OfflineVideo } from "./offline-video";
+
 interface VideoProps {
   hostName: string;
   hostIdentity: string;
 }
 
 export const Video = ({ hostName, hostIdentity }: VideoProps) => {
-  return <div>Video</div>;
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+  const tracks = useTracks([
+    Track.Source.Camera,
+    Track.Source.Microphone,
+  ]).filter((track) => track.participant.identity === hostIdentity);
+  let content;
+  if (!participant && connectionState === ConnectionState.Connected) {
+    content = <OfflineVideo username={hostName} />;
+  } else if (!participant || tracks.length === 0) {
+    content = <p>Loading...</p>;
+  } else {
+    content = <p>Live video</p>;
+  }
+
+  return <div className="aspect-video border-b group relative">{content}</div>;
 };
