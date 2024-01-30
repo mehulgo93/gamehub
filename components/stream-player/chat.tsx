f7dcf2c9672c563db50ca18a6749061413562446
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-01-30 15:17:15 -0500

    working on chat component

---
+++ components/stream-player/chat.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+interface ChatProps {
+  viewerName: string;
+  hostName: string;
+  hostIdentity: string;
+  isFollowing: boolean;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+}
+
+export const Chat = ({
+  viewerName,
+  hostName,
+  hostIdentity,
+  isFollowing,
+  isChatEnabled,
+  isChatDelayed,
+  isChatFollowersOnly,
+}: ChatProps) => {
+  return <div>Chat</div>;
+};
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,7 +3,10 @@
 import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { cn } from "@/lib/utils";
 import { Video } from "./video";
+import { Chat } from "./chat";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -17,6 +20,7 @@ export const StreamPlayer = ({
   isFollowing,
 }: StreamPlayerProps) => {
   const { token, name, identity } = useViewerToken(user.id);
+  const { collapsed } = useChatSidebar((state) => state);
 
   if (!token || !name || !identity) {
     return <div>Cannot watch the stream</div>;
@@ -26,10 +30,24 @@ export const StreamPlayer = ({
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+        className={cn(
+          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+        )}
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollba pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+        <div className={cn("col-span-1", collapsed && "hidden")}>
+          <Chat
+            viewerName={name}
+            hostName={user.username}
+            hostIdentity={user.id}
+            isFollowing={isFollowing}
+            isChatEnabled={stream.isChatEnabled}
+            isChatDelayed={stream.isChatDelayed}
+            isChatFollowersOnly={stream.isChatFollowersOnly}
+          />
         </div>
       </LiveKitRoom>
     </>
