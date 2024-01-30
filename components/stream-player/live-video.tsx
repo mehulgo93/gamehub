b39191d385400e6a46aa8e82351f85647b4deb89
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-01-30 14:54:55 -0500

    finished video component

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Participant, Track } from "livekit-client";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useTracks } from "@livekit/components-react";
 import { FullScreenControl } from "./full-screen-control";
 import { useEventListener } from "usehooks-ts";
@@ -16,6 +16,28 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [volume, setVolume] = useState(0);
+
+  const onVolumeChange = (value: number) => {
+    setVolume(+value);
+    if (videoRef?.current) {
+      videoRef.current.muted = value === 0;
+      videoRef.current.volume = +value * 0.01;
+    }
+  };
+
+  const onToggleMute = () => {
+    const isMuted = volume === 0;
+    setVolume(isMuted ? 50 : 0);
+    if (videoRef?.current) {
+      videoRef.current.muted = !isMuted;
+      videoRef.current.volume = isMuted ? 0.5 : 0;
+    }
+  };
+
+  useEffect(() => {
+    onVolumeChange(0);
+  }, []);
 
   const toggleFullScreen = () => {
     if (isFullScreen) {
@@ -44,7 +66,11 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
       <video ref={videoRef} width="100%" />
       <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
         <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
-          <VolumeControl onChange={() => {}} value={0} onToggle={() => {}} />
+          <VolumeControl
+            onChange={onVolumeChange}
+            value={volume}
+            onToggle={onToggleMute}
+          />
           <FullScreenControl
             isFullScreen={isFullScreen}
             onToggle={toggleFullScreen}
