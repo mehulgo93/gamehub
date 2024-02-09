fd2c6b1a74c18e0643b71119a997c8814190c5d4
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-02-08 20:48:25 -0500

    thumbnail added

--- components/stream-player/info-modal.tsx
+++ components/stream-player/info-modal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { ElementRef, useRef, useState, useTransition } from "react";
 import {
   Dialog,
   DialogClose,
@@ -11,6 +11,13 @@ import {
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { updateStream } from "@/actions/stream";
+import { toast } from "sonner";
+import { UploadDropzone } from "@/lib/uploadthing";
+import { useRouter } from "next/navigation";
+import { Hint } from "../hint";
+import { Trash } from "lucide-react";
+import Image from "next/image";
 
 interface InfoModalProps {
   initialName: string;
@@ -21,7 +28,36 @@ export const InfoModal = ({
   initialName,
   initialThumbnailUrl,
 }: InfoModalProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
   const [name, setName] = useState(initialName);
+  const [thumbnailUrl, setThumbnailUrl] = useState(initialThumbnailUrl);
+  const onRemove = () => {
+    updateStream({ thumbnailUrl: null })
+      .then(() => {
+        toast.success("Thumbnail Removed");
+        setThumbnailUrl("");
+        closeRef?.current?.click();
+      })
+      .catch(() => toast.error("Something went wrong"));
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    startTransition(() => {
+      updateStream({ name: name })
+        .then(() => {
+          toast.success("Stream Update");
+          closeRef?.current?.click();
+        })
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -33,18 +69,62 @@ export const InfoModal = ({
         <DialogHeader>
           <DialogTitle>Edit stream info</DialogTitle>
         </DialogHeader>
-        <form className="space-y-14">
+        <form onSubmit={onSubmit} className="space-y-14">
           <div className="space-y-2">
             <Label>Name</Label>
             <Input
               placeholder="Stream name"
-              onChange={() => {}}
-              disabled={false}
+              onChange={onChange}
+              disabled={isPending}
               value={name}
             />
           </div>
+          <div className="space-y-2">
+            <Label>Thumbnail</Label>
+            {thumbnailUrl ? (
+              <div className="relative aspect-video rounded-xl overflow-hidden border border-white/10">
+                <div className="absolute top-2 right-2 z-[10]">
+                  <Hint label="Remove Thumbnail" asChild side="left">
+                    <Button
+                      type="button"
+                      disabled={isPending}
+                      onClick={onRemove}
+                      className="h-auto w-auto p-1.5"
+                    >
+                      <Trash className="h-4 w-4" />
+                    </Button>
+                  </Hint>
+                </div>
+                <Image
+                  src={thumbnailUrl}
+                  alt="Thumbnail"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            ) : (
+              <div className="rounded-xl border-outline-dashed outline-muted">
+                <UploadDropzone
+                  endpoint="thumbnailUploader"
+                  appearance={{
+                    label: {
+                      color: "#FFFFFF",
+                    },
+                    allowedContent: {
+                      color: "#FFFFFF",
+                    },
+                  }}
+                  onClientUploadComplete={(res) => {
+                    setThumbnailUrl(res?.[0]?.url);
+                    router.refresh();
+                    closeRef?.current?.click();
+                  }}
+                />
+              </div>
+            )}
+          </div>
           <div className="flex justify-between">
-            <DialogClose asChild>
+            <DialogClose ref={closeRef} asChild>
               <Button variant="ghost" type="button">
                 Cancel
               </Button>
