946742e48e0a7fff1466e3e011065fe2d6cff22c
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-02-10 20:35:09 -0500

    isLive not working

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -1,29 +1,45 @@
-import { db } from "./db"
+import { db } from "@/lib/db"
 
 export const getUserByUsername = async (username: string) => {
-    const user = await db.user.findUnique({
-        where: {
-            username,
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+      externalUserId: true,
+      username: true,
+      bio: true,
+      imageUrl: true,
+      stream: {
+        select: {
+          id: true,
+          isLive: true,
+          isChatDelayed: true,
+          isChatEnabled: true,
+          isChatFollowersOnly: true,
+          thumbnailUrl: true,
+          name: true,
         },
-        include: {
-            stream: true,
-            _count: {
-                select: {
-                    followedBy: true,
-                }
-            }
+      },
+      _count: {
+        select: {
+          followedBy: true,
         },
-    });
+      },
+    },
+  });
 
-    return user;
-}
+  return user;
+};
 
 export const getUserById = async (id: string) => {
-    const user = await db.user.findUnique({
-        where: {id},
-        include: {
-            stream: true,
-        },
-    })
-    return user;
+  const user = await db.user.findUnique({
+    where: { id },
+    include: {
+      stream: true,
+    },
+  });
+
+  return user;
 };
