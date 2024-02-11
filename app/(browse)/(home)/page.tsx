3111380923245a9de9e2c43894adfc1fbe4662b5
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-02-10 19:44:35 -0500

    almost done with the code

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
+import { Suspense } from "react";
+
+import { Results, ResultsSkeleton } from "./_components/results";
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1>Home</h1>
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
     </div>
   );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ const notFoundPage = () => {
   return (
     <div className="h-full flex flex-col space-y-4 items-center justify-center text-muted-foreground">
       <h1 className="text-4xl">404</h1>
-      <p className="">We couldn&apos;t find the user you were looking for.</p>
+      <p className="">We couldn&apos;t find the page you were looking for.</p>
       <Button variant="secondary" asChild>
         <Link href="/">Go back home</Link>
       </Button>
