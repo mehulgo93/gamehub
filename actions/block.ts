66966b3327f41e1f44062ccfc35f894017ea35fb
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-02-12 13:58:44 -0500

    end of development

--- actions/block.ts
+++ actions/block.ts
@@ -1,46 +1,43 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
 
 import { getSelf } from "@/lib/auth-service";
 import { blockUser, unblockUser } from "@/lib/block-service"
-import { RoomServiceClient } from "livekit-server-sdk";
-import { revalidatePath } from "next/cache";
-
 
 const roomService = new RoomServiceClient(
-    process.env.LIVEKIT_API_URL!,
-    process.env.LIVEKIT_API_KEY!,
-    process.env.LIVEKIT_API_SECRET!,
-)
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!,
+);
 
-export const onBlock = async(id: string) => {
-    const self = await getSelf();
-    let blockedUser;
-    try {
-        blockedUser = await blockUser(id);
-    } catch {
+export const onBlock = async (id: string) => {
+  const self = await getSelf();
 
-    }
-    try {
-        await roomService.removeParticipant(self.id, id)
-    } catch {
-        // this means user is not in the room
-    }
+  let blockedUser;
 
-    revalidatePath(`/u/${self.username}/community`);
+  try {
+    blockedUser = await blockUser(id);
+  } catch {
+    // This means user is a guest
+  }
 
-    return blockedUser;
-}
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch {
+    // This means user is not in the room
+  }
 
+  revalidatePath(`/u/${self.username}/community`);
 
-export const onUnBlock = async(id: string) => {
-    const unblockedUser = await unblockUser(id);
+  return blockedUser;
+};
 
-    revalidatePath("/")
+export const onUnblock = async (id: string) => {
+  const self = await getSelf();
+  const unblockedUser = await unblockUser(id);
 
-    if (unblockedUser) {
-        revalidatePath(`/${unblockedUser.blocked.username}`);
-    }
-
-    return unblockedUser;
-}
+  revalidatePath(`/u/${self.username}/community`);
+  return unblockedUser;
+};
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,8 +1,9 @@
-import { StreamPlayer } from "@/components/stream-player";
-import { isBlockedByUser } from "@/lib/block-service";
-import { isFollowingUser } from "@/lib/follow-service";
-import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { isFollowingUser } from "@/lib/follow-service";
+import { isBlockedByUser } from "@/lib/block-service";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface UserPageProps {
   params: {
@@ -10,11 +11,13 @@ interface UserPageProps {
   };
 }
 
-const userPage = async ({ params }: UserPageProps) => {
+const UserPage = async ({ params }: UserPageProps) => {
   const user = await getUserByUsername(params.username);
+
   if (!user || !user.stream) {
     notFound();
   }
+
   const isFollowing = await isFollowingUser(user.id);
   const isBlocked = await isBlockedByUser(user.id);
 
@@ -27,4 +30,4 @@ const userPage = async ({ params }: UserPageProps) => {
   );
 };
 
-export default userPage;
+export default UserPage;
